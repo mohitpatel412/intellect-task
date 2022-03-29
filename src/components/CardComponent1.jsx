d9fc46c2f9466be942b4b5b22261c0b753f174c9
Mohit Patel <mohitpatel040102@gmail.com>
2022-03-29 23:42:59 +0530

    Initial commit

---
+++ src/components/CardComponent1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardComponent1 = () => {
+  return (
+    <div>
+      <p>CardComponent1</p>
+    </div>
+  );
+};
+
+export default CardComponent1;
---
+++ src/components/CardComponent2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardComponent2 = () => {
+  return (
+    <div>
+      <p>CardComponent2</p>
+    </div>
+  );
+};
+
+export default CardComponent2;
